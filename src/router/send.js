d582a7282ee471836c10b15893bb7104f7e33d42
otomad <shuishenme@outlook.com>
2024-01-07 23:29:29 +0800

    新增 紧急清屏

--- src/router/send.js
+++ src/router/send.js
@@ -3,8 +3,8 @@ import { socket } from "../www.js";
 export default async function handleSendMessage(request, response) {
 	const url = new URL(request.url, `http://${request.headers.host}`);
 	if (request.method === "GET" && url.pathname === "/api/send") {
-		const msg = url.searchParams.get("msg");
-		if (msg) {
+		const msg = url.searchParams.get("msg") ?? "";
+		if (msg !== undefined) {
 			let clientCount = 0;
 			socket.clients.forEach(client => {
 				if (client.readyState === 1) {
